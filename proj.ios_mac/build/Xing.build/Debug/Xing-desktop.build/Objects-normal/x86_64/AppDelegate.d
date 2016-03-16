c0dd0ae43206610501a2bb103c8169e72466e890
Emily Xing <xfy@me.com>
2016-03-15 20:59:33 -0700

    initial commit

---
+++ proj.ios_mac/build/Xing.build/Debug/Xing-desktop.build/Objects-normal/x86_64/AppDelegate.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/Emily/Documents/CocosProjects/Xing/Classes/AppDelegate.cpp \
+  /Users/Emily/Documents/CocosProjects/Xing/Classes/AppDelegate.h \
+  /Users/Emily/Documents/CocosProjects/Xing/Classes/HelloWorldScene.h
---
+++ proj.ios_mac/build/Xing.build/Debug/Xing-desktop.build/Objects-normal/x86_64/main.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/Emily/Documents/CocosProjects/Xing/proj.ios_mac/mac/main.cpp \
+  /Users/Emily/Documents/CocosProjects/Xing/Classes/AppDelegate.h
